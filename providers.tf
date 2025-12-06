163d26a0770a32fa97d52c836baac89ac46d6d9a
Maheshbharambe45 <bharambemahesh7@gmail.com>
2025-12-06 12:39:19 +0530

    Updating  Infrastructure Code

--- providers.tf
+++ providers.tf
@@ -7,12 +7,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0" 
     }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.7.1"
-    }
-
+    
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1.0"
@@ -21,16 +16,6 @@ terraform {
     local = {
       source  = "hashicorp/local"
       version = "~> 2.1.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.1.0"
-    }
-
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
     }
   }
 }
